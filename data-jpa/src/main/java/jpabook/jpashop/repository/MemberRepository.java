533d3eec5110521350bc87fc780c076677aaa3a1
yujindonut <bbb3631@naver.com>
2023-03-09 01:38:41 +0900

    feat: @Query, 값, DTO 조회하기

--- data-jpa/src/main/java/jpabook/jpashop/repository/MemberRepository.java
+++ data-jpa/src/main/java/jpabook/jpashop/repository/MemberRepository.java
@@ -2,6 +2,7 @@ package jpabook.jpashop.repository;
 
 import java.util.List;
 import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.dto.MemberDto;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -16,4 +17,8 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
 
     @Query("select m from Member m where m.username= :username and m.age = :age")
     List<Member> findUser(@Param("username") String username, @Param("age") int age);
+
+    @Query("select new jpabook.jpashop.dto.MemberDto(m.id, m.username, t.name) " +
+            "from Member m join m.team t")
+    List<MemberDto> findMemberDto();
 }
