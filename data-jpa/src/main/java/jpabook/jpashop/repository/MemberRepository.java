aa923e44f45ae0845cc16d1eedd4270b2a5b5aa3
yujindonut <bbb3631@naver.com>
2023-03-09 01:24:15 +0900

    feat: 메소드 이름으로 쿼리생성

--- data-jpa/src/main/java/jpabook/jpashop/repository/MemberRepository.java
+++ data-jpa/src/main/java/jpabook/jpashop/repository/MemberRepository.java
@@ -1,8 +1,9 @@
-package jpabook.jpashop;
+package jpabook.jpashop.repository;
 
+import java.util.List;
 import jpabook.jpashop.domain.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
-
+    List<Member> findByUsernameAndAgeGreaterThan(String username, int age);
 }
---
+++ data-jpa/src/main/java/jpabook/jpashop/repository/TeamRepository.java
@@ -0,0 +1,8 @@
+package jpabook.jpashop.repository;
+
+import jpabook.jpashop.domain.Team;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TeamRepository extends JpaRepository<Team, Long> {
+
+}
