cfa0e46442c681ba604da193f79e8f2c81f0dd3f
yujindonut <bbb3631@naver.com>
2023-03-16 01:33:32 +0900

    feat: 페이징과 정렬

--- data-jpa/src/main/java/jpabook/jpashop/controller/MemberController.java
+++ data-jpa/src/main/java/jpabook/jpashop/controller/MemberController.java
@@ -1,8 +1,11 @@
 package jpabook.jpashop.controller;
 
 import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.dto.MemberDto;
 import jpabook.jpashop.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,6 +23,11 @@ public class MemberController {
     @GetMapping("/members/{id}")
     public String findMember(@PathVariable("id") Member member) {
         return member.getUsername();
+    }
+
+    @GetMapping("/members")
+    public Page<MemberDto> list(Pageable pageable) {
+        return memberRepository.findAll(pageable).map(MemberDto::new);
     }
 }
 
